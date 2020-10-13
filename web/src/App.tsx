cdcddda1e4f98148ca1e7a7963a5ce124d7d5bd7
João Vitor <j.vitor.mf@gmail.com>
2020-10-12 22:42:38 -0300

    feat: add landing and map pages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
+import Routes from './routes';
+
+import './styles/global.css';
+
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Routes />
   );
 }
 
